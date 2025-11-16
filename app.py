0f8e91e33090a6eb55041ea38a53b2b8f384bcfa
Hermes80 <saucerchris11@gmail.com>
2025-11-15 18:13:11 -0800

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def login_required(f):
 # MOCK DATA (SHAPED FOR REAL APIS LATER)
 
     def get_dashboard_data():
+        def get_dashboard_data():
     from ebay_api import get_active_listings, get_orders
 
     # Get eBay data
@@ -66,7 +67,7 @@ def login_required(f):
         "ai_command_log": ai_command_log[-20:]
     }
     return data
-
+    
 # ---------------------------
 # ROUTES
 # ---------------------------
