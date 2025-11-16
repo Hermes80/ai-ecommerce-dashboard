4e0b0f181f869a36ec70d3245e18871bc1ca7d7e
Hermes80 <saucerchris11@gmail.com>
2025-11-15 20:45:00 -0800

    Create ai_settings.py

---
+++ ai_settings.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+SETTINGS_FILE = "ai_settings.json"
+
+DEFAULT_SETTINGS = {
+    "auto_reprice": False,
+    "auto_list": False,
+    "auto_source": False,
+    "auto_order": False,
+    "auto_inventory_sync": False,
+    "auto_fulfill": False,
+    "auto_message": False,
+    "auto_predict": False,
+    "auto_refund": False
+}
+
+def load_settings():
+    if not os.path.exists(SETTINGS_FILE):
+        save_settings(DEFAULT_SETTINGS)
+        return DEFAULT_SETTINGS
+    with open(SETTINGS_FILE, "r") as f:
+        return json.load(f)
+
+def save_settings(settings):
+    with open(SETTINGS_FILE, "w") as f:
+        json.dump(settings, f, indent=4)
+
+def update_setting(key, value):
+    settings = load_settings()
+    settings[key] = value
+    save_settings(settings)
+    return settings
