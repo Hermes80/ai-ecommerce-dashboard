ffb85cf16721a589068b862ae44c20cedfa2ea98
Hermes80 <saucerchris11@gmail.com>
2025-11-23 17:11:49 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 import os
 
-# Load .env file
 load_dotenv()
 
 EBAY_APP_ID = os.getenv("EBAY_APP_ID")
@@ -10,3 +9,6 @@ EBAY_REDIRECT_URI = os.getenv("EBAY_REDIRECT_URI")
 
 EBAY_OAUTH_URL = os.getenv("EBAY_OAUTH_URL", "https://api.ebay.com/identity/v1/oauth2/token")
 EBAY_REST_URL = os.getenv("EBAY_REST_URL", "https://api.ebay.com")
+
+# These are optional but recommended
+EBAY_SCOPE = "https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/sell.inventory"
